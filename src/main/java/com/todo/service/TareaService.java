06d85e37c170b582b6f12a27fb348fcdb98d83a2
fmalmada <fmalmada@fi.uba.ar>
2023-11-27 20:41:57 -0300

    postTarea

---
+++ src/main/java/com/todo/service/TareaService.java
@@ -0,0 +1,10 @@
+package com.todo.service;
+
+import com.todo.dto.TareaDTO;
+import com.todo.dto.TareaPostDTO;
+
+public interface TareaService {
+    
+    public TareaDTO postTarea(TareaPostDTO tarea);
+    
+}
