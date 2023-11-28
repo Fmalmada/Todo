ed99139f797680f2dba8431552c30a062770dd3a
fmalmada <fmalmada@fi.uba.ar>
2023-11-27 21:03:28 -0300

    Get

--- src/main/java/com/todo/service/TareaService.java
+++ src/main/java/com/todo/service/TareaService.java
@@ -1,10 +1,16 @@
 package com.todo.service;
 
+import java.util.List;
+
 import com.todo.dto.TareaDTO;
 import com.todo.dto.TareaPostDTO;
 
 public interface TareaService {
     
     public TareaDTO postTarea(TareaPostDTO tarea);
+
+    public TareaDTO getTarea(Long id);
+
+    public List<TareaDTO> getTareas();
     
 }
