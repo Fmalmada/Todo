cd8a00334ee815a35826fe3cd4e4921b0c2c8f93
fmalmada <fmalmada@fi.uba.ar>
2023-11-29 21:57:17 -0300

    Eliminar importaciones sin usar

--- src/main/java/com/todo/dto/TareaDTO.java
+++ src/main/java/com/todo/dto/TareaDTO.java
@@ -1,9 +1,6 @@
 package com.todo.dto;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
